ca9ac8368acb81b3ffe5658178f0e8d3fefd9aa8
Armando Arce <armando.arce@gmail.com>
2020-03-05 18:53:07 -0500

    Update GraphicsBenchData.js

--- GraphicsBenchData.js
+++ GraphicsBenchData.js
@@ -11,6 +11,87 @@ function setup() {
 	data = data.concat(localStorage.getItem("GraphicsBenchArc-100000"));
 	data = data.concat(localStorage.getItem("GraphicsBenchArc-500000"));
 	
+	data = data.concat(localStorage.getItem("GraphicsBenchCircle-100"));
+	data = data.concat(localStorage.getItem("GraphicsBenchCircle-500"));
+	data = data.concat(localStorage.getItem("GraphicsBenchCircle-1000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchCircle-5000"));
+    data = data.concat(localStorage.getItem("GraphicsBenchCircle-10000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchCircle-50000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchCircle-100000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchCircle-500000"));
+	
+	data = data.concat(localStorage.getItem("GraphicsBenchFillArc-100"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillArc-500"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillArc-1000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillArc-5000"));
+    data = data.concat(localStorage.getItem("GraphicsBenchFillArc-10000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillArc-50000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillArc-100000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillArc-500000"));
+
+	data = data.concat(localStorage.getItem("GraphicsBenchFillCircle-100"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillCircle-500"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillCircle-1000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillCircle-5000"));
+    data = data.concat(localStorage.getItem("GraphicsBenchFillCircle-10000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillCircle-50000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillCircle-100000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillCircle-500000"));
+	
+	data = data.concat(localStorage.getItem("GraphicsBenchFillRect-100"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillRect-500"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillRect-1000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillRect-5000"));
+    data = data.concat(localStorage.getItem("GraphicsBenchFillRect-10000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillRect-50000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillRect-100000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillRect-500000"));
+
+	data = data.concat(localStorage.getItem("GraphicsBenchFillShape-100"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillShape-500"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillShape-1000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillShape-5000"));
+    data = data.concat(localStorage.getItem("GraphicsBenchFillShape-10000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillShape-50000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillShape-100000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchFillShape-500000"));
+	
+	data = data.concat(localStorage.getItem("GraphicsBenchLine-100"));
+	data = data.concat(localStorage.getItem("GraphicsBenchLine-500"));
+	data = data.concat(localStorage.getItem("GraphicsBenchLine-1000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchLine-5000"));
+    data = data.concat(localStorage.getItem("GraphicsBenchLine-10000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchLine-50000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchLine-100000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchLine-500000"));
+
+	data = data.concat(localStorage.getItem("GraphicsBenchRect-100"));
+	data = data.concat(localStorage.getItem("GraphicsBenchRect-500"));
+	data = data.concat(localStorage.getItem("GraphicsBenchRect-1000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchRect-5000"));
+    data = data.concat(localStorage.getItem("GraphicsBenchRect-10000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchRect-50000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchRect-100000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchRect-500000"));
+
+	data = data.concat(localStorage.getItem("GraphicsBenchShape-100"));
+	data = data.concat(localStorage.getItem("GraphicsBenchShape-500"));
+	data = data.concat(localStorage.getItem("GraphicsBenchShape-1000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchShape-5000"));
+    data = data.concat(localStorage.getItem("GraphicsBenchShape-10000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchShape-50000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchShape-100000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchShape-500000"));
+				
+	data = data.concat(localStorage.getItem("GraphicsBenchText-100"));
+	data = data.concat(localStorage.getItem("GraphicsBenchText-500"));
+	data = data.concat(localStorage.getItem("GraphicsBenchText-1000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchText-5000"));
+    data = data.concat(localStorage.getItem("GraphicsBenchText-10000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchText-50000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchText-100000"));
+	data = data.concat(localStorage.getItem("GraphicsBenchText-500000"));
+			
 	var filename = 'console.json'
     var blob = new Blob([data], {type: 'text/json'}),
     e    = document.createEvent('MouseEvents'),
