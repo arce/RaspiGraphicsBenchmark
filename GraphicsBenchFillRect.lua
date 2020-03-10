bcc4f657c9cf6f8e3bced2ec833dcf779619ba87
Armando Arce <armando.arce@gmail.com>
2020-03-10 13:02:35 -0400

    Update

--- GraphicsBenchFillRect.lua
+++ GraphicsBenchFillRect.lua
@@ -11,6 +11,19 @@ local l = 10
 local xr = 1920
 local yr = 1080
 local t
+local colors =
+{{0xa6,0xce,0xe3},
+{0x1f,0x78,0xb4},
+{0xb2,0xdf,0x8a},
+{0x3,0x3a,0x02c},
+{0xfb,0x9a,0x99},
+{0xe,0x31,0xa1c},
+{0xfd,0xbf,0x6f},
+{0xff,0x7f,0x00},
+{0xca,0xb2,0xd6},
+{0x6a,0x3d,0x9a},
+{0xff,0xff,0x99},
+{0xb1,0x59,0x28}}
 
 function setup()
   size(xr,yr)
@@ -28,7 +41,7 @@ function setup()
     y[i] = math.random()*yr
     w[i] = math.random()*xr/10
     h[i] = math.random()*yr/10
-    c[i] = color(math.random()*255,math.random()*255,math.random()*255,0)
+    c[i] = math.random(1,12)
   end
 end
 
@@ -39,7 +52,7 @@ function draw()
     t[m] = time()
     background(255)
     for i=0,n-1 do
-      fill(c[i])
+      fill(colors[c[i]][1],colors[c[i]][2],colors[c[i]][3])
       rect(x[i],y[i],w[i],h[i])
     end
   end
--- diokol_batchRect.sh
+++ diokol_batchRect.sh
@@ -1,3 +1,3 @@
 rm salida_lua.csv
 
-../bin/Diököl GraphicsBenchRect.lua 100000 >> salida_lua.csv
+../Diokol/bin/macos/Diököl GraphicsBenchFillRect.lua 100000 >> salida_lua.csv
